1c6fe202d1cbd132b24b2153cb2fcb0e76fdd125
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-09-11 17:06:53 +0100

    feat: implement utils for account

---
+++ src/utils/account.ts
@@ -0,0 +1,41 @@
+const liskBrowserClient = require("@liskhq/lisk-client/browser");
+
+const cryptography = liskBrowserClient.cryptography;
+
+interface Account {
+  address: string;
+  passphrase: string;
+}
+
+const LOCAL_STORAGE_KEY = "USER_ACCOUNT";
+
+export const setupAccount = async (passphrase: string) => {
+  const address = await getAddressFromPassphrase(passphrase);
+  const account = {
+    address: address,
+    passphrase: passphrase,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(account));
+};
+
+export const getAccount = (): Account | null => {
+  const account = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (!account) return null;
+
+  return JSON.parse(account) as Account;
+};
+
+export const extractPrivateKey = async (passphrase: string): Promise<Buffer> => {
+  return await cryptography.ed.getPrivateKeyFromPhraseAndPath(
+    passphrase,
+    "m/44'/134'/0'"
+  );
+};
+export const getAddressFromPassphrase = async (passphrase: string): Promise<string> => {
+  const privateKey = await extractPrivateKey(passphrase);
+
+  const addressBuffer = cryptography.address.getAddressFromPrivateKey(privateKey);
+  const address = cryptography.address.getLisk32AddressFromAddress(addressBuffer);
+
+  return address;
+};
