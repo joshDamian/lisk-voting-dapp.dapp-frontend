cf28c6b9016280d6a6fb8f3ae8dbb335201ea830
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-09-11 17:07:51 +0100

    feat: add util to connect to lisk client

---
+++ src/utils/getLiskClient.ts
@@ -0,0 +1,17 @@
+import { apiClient } from "@liskhq/lisk-client";
+const liskBrowserClient = require("@liskhq/lisk-client/browser");
+
+type ApiClient = Awaited<ReturnType<typeof apiClient.createWSClient>>;
+
+const apiClientHelper = liskBrowserClient.apiClient;
+const apiUrl = "ws://localhost:7887/rpc-ws";
+
+let clientInstance: ApiClient | undefined;
+
+export const getLiskClient = async () => {
+  if (clientInstance) return clientInstance;
+
+  clientInstance = await apiClientHelper.createWSClient(apiUrl);
+
+  return clientInstance;
+};
