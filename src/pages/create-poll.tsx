f21e3d09aef12b401cbfe0020f733c4fb39b0e10
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-09-12 02:47:37 +0100

    feat: add back link

--- src/pages/create-poll.tsx
+++ src/pages/create-poll.tsx
@@ -3,11 +3,17 @@ import { NextPageWithLayout } from "./_app";
 import { Layout } from "@/components/layout";
 import { createPoll } from "@/api/poll";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CreatePollPage: NextPageWithLayout = () => {
   const router = useRouter();
   return (
     <div>
+      <div className="mb-6">
+        <Link className="underline" href={"/"}>
+          {"< See All Polls"}
+        </Link>
+      </div>
       <h3 className="mb-8 text-xl lg:text-4xl text-center">Create A Poll</h3>
       <CreatePoll
         createPoll={async (payload) => {
